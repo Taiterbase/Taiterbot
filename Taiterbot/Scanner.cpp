432fa22d09ef82f5d285dc869c7daefc39874f58
Taite Nazifi <taitenazifi@gmail.com>
2019-12-12 11:17:54 -0700

    Update Scanner.cpp

--- Taiterbot/Scanner.cpp
+++ Taiterbot/Scanner.cpp
@@ -1,17 +1,12 @@
 #include "Scanner.h"
-/*
-Creates a scanner object that initializes the middle points of the verticle and horizontal
-of the screen, effectively grabbing the center point of the game window. 
-*/
+
 Scanner::Scanner(int sCenterX, int sCenterY, HDC hdc = nullptr) {
 	_sCenterX = sCenterX;
 	_sCenterY = sCenterY;
 	_hdc = hdc;
 }
 
-/**
-	Simulates a left click down by using mouse events.
-*/
+
 void Scanner::LeftClickDown()
 {
 	input = { 0 };
@@ -21,9 +16,7 @@ void Scanner::LeftClickDown()
 	::SendInput(1, &input, sizeof(INPUT));
 }
 
-/**
-	Simulates left click up using mouse events.
-*/
+
 void Scanner::LeftClickUp()
 {
 	input = { 0 };
@@ -35,27 +28,13 @@ void Scanner::LeftClickUp()
 }
 
 
-/*Check the color at the middle of the screen, shoot if it's what we want, using
-	mouse events and reading the color of rgb values.
-*/
 void Scanner::ScanReticle() {
 	while (true) {
-
-		/* Draw a simple blue rectangle on the desktop */
-		//getRGBavg(1255, 690, 1305, 750);
-		
-		/*
-		 Draw a simple blue rectangle on the desktop 
-		 This can be used for debugging purposes, or just to see
-		 where the calculated mid point of the window is...
-		*/
 		//HDC hdthing = GetDC(0);
 		//RECT rect = { _sCenterX - 10, _sCenterY - 10, _sCenterX + 10, _sCenterY + 10 };
 		//HBRUSH blueBrush = CreateSolidBrush(RGB(255, 0, 0));
 		//FillRect(hdthing, &rect, blueBrush);
 		
-
-		//Performs left click if the reticle's red value is >=250.
 		if ((1 << 15) & GetAsyncKeyState(0xA2)) { //left ctrl is the trigger key.
 			int r = (int)GetRValue(GetPixel(_hdc, _sCenterX, _sCenterY));
 			if(r >= 250){
